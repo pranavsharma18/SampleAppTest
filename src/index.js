9926cd938bee84589295973010e416067c288ef0
pranavsharma18 <trainee03@omniesolutions.com>
2019-08-14 12:02:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+// import * as serviceWorker from "./serviceWorker";
+import Code from "./lib/code";
+
+ReactDOM.render(<Code />, document.getElementById("root"));
---
+++ src/lib/code.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Code extends Component {
+    state = {}
+    render() {
+        return (<h1>Code</h1>);
+    }
+}
+
+export default Code;
